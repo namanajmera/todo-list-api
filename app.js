51cd04021c1d0368487228d01eff8baf94841804
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-11-14 17:46:30 +0530

    Added: Basic Get Request of Getting the todos.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
+const fs = require('fs');
 const app = express();
 const port = 8080;
 
-app.use("/", (req, res) => {
-  res.send("Todo List");
+const data = fs.readFileSync('./data/todos.js','utf-8');
+const todos = JSON.parse(data);
+
+app.get('/todos', (req,res) => {
+  res.status(200).json({
+    message: "success",
+    result: todos.length,
+    data: todos
+  })
+}) 
+
+app.use('/', (req,res) => {
+  res.send('Hello From Todos.')
 });
+
 app.listen(port, () => {
-  console.log(`Server is running on ${port} port`);
+  console.log(`Server is running on ${"http://localhost:" + port}`);
 });
