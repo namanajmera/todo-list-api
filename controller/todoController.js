86c4cb1e934f05bd3b254ff3ae535cf54b8f5ec4
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-11-20 20:46:24 +0530

    Added: All request connected by DB.

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,73 +1,11 @@
 const Todo = require("../models/todoModel");
 
-const data = [
-  {
-    id: "1",
-    todo_name: "Read Dune",
-    isCompleted: "true",
-    create_date: "2020-01-05",
-  },
-  {
-    id: "2",
-    todo_name: "Finish Foundation Trilogy",
-    isCompleted: "true",
-    create_date: "2020-02-10",
-  },
-  {
-    id: "3",
-    todo_name: "Explore The Expanse series",
-    isCompleted: "false",
-    create_date: "2020-03-15",
-  },
-  {
-    id: "4",
-    todo_name: "Read Neuromancer",
-    isCompleted: "true",
-    create_date: "2020-04-20",
-  },
-  {
-    id: "5",
-    todo_name: "Discover Ender's Game",
-    isCompleted: "false",
-    create_date: "2020-05-25",
-  },
-  {
-    id: "6",
-    todo_name: "Finish Hyperion Cantos",
-    isCompleted: "false",
-    create_date: "2020-06-30",
-  },
-  {
-    id: "7",
-    todo_name: "Explore The Martian",
-    isCompleted: "true",
-    create_date: "2020-07-04",
-  },
-  {
-    id: "8",
-    todo_name: "Read Snow Crash",
-    isCompleted: "true",
-    create_date: "2020-08-09",
-  },
-  {
-    id: "9",
-    todo_name: "Finish 1984",
-    isCompleted: "false",
-    create_date: "2020-09-14",
-  },
-  {
-    id: "10",
-    todo_name: "Discover The Hitchhiker's Guide to the Galaxy",
-    isCompleted: "true",
-    create_date: "2020-10-19",
-  },
-];
-
-exports.getTodo = (req, res, next) => {
+exports.getTodo = async (req, res, next) => {
+  const todos = await Todo.find();
   res.status(200).json({
     message: "success",
-    result: data.length,
-    data: data,
+    result: todos.length,
+    data: todos,
   });
 };
 
@@ -81,9 +19,9 @@ exports.postTodo = async (req, res, next) => {
   });
 };
 
-exports.getTourById = (req,res,next) => {
+exports.getTourById = async (req,res,next) => {
     const id = req.params.id;
-    const todo = data.filter((ele) => ele.id === id);
+    const todo = await Todo.findById(id);
     res.status(200).json({
         status: "success",
         data: todo 
